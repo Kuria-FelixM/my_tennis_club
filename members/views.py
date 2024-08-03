7ab542ec4d12ebb922f6e3f0be82c51e4cbe33b1
KURIA FELIX MUHIA <160228937+Kuria-FelixM@users.noreply.github.com>
2024-08-03 23:18:27 +0300

    Update views.py

--- members/views.py
+++ members/views.py
@@ -1,9 +1,9 @@
 from django.template import loader
 from django.http import HttpResponse
 
-# views
+# Create your views here.
 def memberss(request):
     template = loader.get_template('myfirst.html')
     return HttpResponse(template.render())
 
-# Create your views here.
+
